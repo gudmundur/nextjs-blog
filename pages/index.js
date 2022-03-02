cb2252fcf7cf0671f91c829eee57019c290d5b84
Gudmundur Bjarni Olafsson <gudmundur.bjarni@gmail.com>
2022-03-02 14:48:33 +0100

    Small edit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to my <a href="https://nextjs.org">Next.js!</a> page
         </h1>
 
         <p className="description">
